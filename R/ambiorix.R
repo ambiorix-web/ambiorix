80fc06d8ec49596f28b3748e68542ff0c2274fa8
JohnCoene <jcoenep@gmail.com>
2020-09-10 10:21:09 +0200

    add POST

--- R/ambiorix.R
+++ R/ambiorix.R
@@ -16,10 +16,9 @@ Ambiorix <- R6::R6Class(
     initialize = function(host = "0.0.0.0", port = 3000L){
       private$.host <- host
       private$.port <- port
-      private$.calls <- CallStack$new()
       self$not_found <- response_404
     },
-#' @details Routes
+#' @details GET Method
 #' 
 #' Add routes to listen to.
 #' 
@@ -29,7 +28,20 @@ Ambiorix <- R6::R6Class(
     get = function(path, fun){
       assert_that(valid_path(path))
       assert_that(not_missing(fun))
-      private$.routes[[uuid()]] <- list(path = path, fun = fun)
+      private$.routes[[uuid()]] <- list(path = path, fun = fun, method = "GET")
+      invisible(self)
+    },
+#' @details POSTT Method
+#' 
+#' Add routes to listen to.
+#' 
+#' @param path Route to listen to.
+#' @param fun Function that accepts the request and returns an object 
+#' describing an httpuv response, e.g.: [response()].
+    post = function(path, fun){
+      assert_that(valid_path(path))
+      assert_that(not_missing(fun))
+      private$.routes[[uuid()]] <- list(path = path, fun = fun, method = "POST")
       invisible(self)
     },
 #' @details Start 
@@ -60,7 +72,7 @@ Ambiorix <- R6::R6Class(
 
       # loop over routes
       for(i in 1:length(private$.routes)){
-        if(private$.routes[[i]]$path == req$PATH_INFO){
+        if(private$.routes[[i]]$path == req$PATH_INFO && private$.routes[[i]]$method == req$REQUEST_METHOD){
           cli::cli_alert_success("GET 127.0.0.1:{private$.port}{req$PATH_INFO}")
           return(private$.routes[[i]]$fun(req))
         }
