2de5d91aaf98baf187303695dca588da8d76c50d
JohnCoene <jcoenep@gmail.com>
2020-09-12 11:56:51 +0200

    add response_file

--- R/response.R
+++ R/response.R
@@ -23,7 +23,7 @@ response_404 <- function(body = "Not found", headers = list('Content-Type' = 'te
 #' 
 #' Use HTML templates to render HTTP responses.
 #' 
-#' @param template Name of HTML template file to use (without extension), 
+#' @param file Name of HTML template file to use (without extension), 
 #' looks up the name of the file in `templates` directory.
 #' @param data Named list of data objects to place in `[% tags %]` found in template.
 #' @param status Status of response.
@@ -32,20 +32,40 @@ response_404 <- function(body = "Not found", headers = list('Content-Type' = 'te
 #' \dontrun{response_render("homepage", list(title = "Home!"))}
 #' 
 #' @export
-response_render <- function(template, data = list(), status = 200L){
-  assert_that(not_missing(template))
+response_render <- function(file, data = list(), status = 200L){
+  assert_that(not_missing(file))
 
-  template_path <- make_template_path(template)
-  template_content <- readLines(template_path)
+  file_path <- make_template_path(file)
+  file_content <- readLines(file_path)
 
   for(i in 1:length(data)){
     pattern <- sprintf("\\[%% ?%s ?%%\\]", names(data)[i]) # [% mustache %]
-    template_content <- gsub(pattern, seralise(data[[i]]), template_content)
+    file_content <- gsub(pattern, seralise(data[[i]]), file_content)
   }
 
-  template_content <- paste0(template_content, collapse = "")
+  file_content <- paste0(file_content, collapse = "")
 
-  response(template_content)
+  response(file_content)
+}
+
+#' File Response
+#' 
+#' Use an HTML file as response, this is similar to [response_render()]: 
+#' it does not support `[% tags %]` but is more efficient.
+#' 
+#' @inheritParams response_render
+#' 
+#' @examples
+#' \dontrun{response_file("homepage")}
+#' 
+#' @export
+response_file <- function(file){
+  assert_that(not_missing(file))
+
+  file_path <- make_template_path(file)
+  file_content <- readLines(file_path)
+
+  response(file_content)
 }
 
 #' Make Template Path
