28509ab09f60d557d8c6a46449812236fe0c1fe4
JohnCoene <jcoenep@gmail.com>
2020-09-18 22:43:31 +0200

    add JSON parser

--- R/parser.R
+++ R/parser.R
@@ -3,13 +3,21 @@
 #' Collection of parsers to translate request data.
 #' 
 #' @param req The request object.
+#' @param ... Additional arguments passed to the internal parsers.
 #' 
 #' @section Functions: 
-#' - [parse_multipart()]: Parse `multipart/form-data`.
+#' - [parse_multipart()]: Parse `multipart/form-data` using [mime::parse_multipart()].
+#' - [parse_json()]: Parse `multipart/form-data` using [jsonlite::fromJSON()].
 #' 
+#' @name parsers
 #' @export 
-parse_multipart <- function(req){
+parse_multipart <- function(req, ...){
   check_installed("mime")  
 
-  mime::parse_multipart(req$body)
+  mime::parse_multipart(req$body, ...)
+}
+
+#' @rdname parsers
+parse_json <- function(req, ...){
+  jsonlite::fromJSON(req$body()$read(), ...)
 }
