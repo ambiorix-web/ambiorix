df3de3c81aa5221fdf3e7bc3de41a58cd2a9e597
John Coene <jcoenep@gmail.com>
2022-03-27 11:39:56 +0200

    test: more cookie tests #48

--- tests/testthat/test-cookie.R
+++ tests/testthat/test-cookie.R
@@ -1,8 +1,10 @@
 test_that("Request cookie", {
+  # cookie set
   req <- mockRequest(
     cookie = "yummy_cookie=choco; tasty_cookie=strawberry"
   )
   expect_s3_class(req, "Request")
+  expect_length(req$cookie, 2L)
   expect_equal(
     req$cookie,
     list(
@@ -10,4 +12,37 @@ test_that("Request cookie", {
       tasty_cookie = "strawberry"
     )
   )
+
+  # empty coookie
+  req <- mockRequest(cookie = "")
+  expect_length(req$cookie, 0L)
+  expect_type(req$cookie, "list")
+  expect_equal(req$cookie, list())
+
+  # incorrect cookie
+  req <- mockRequest(
+    cookie = "yummy_cookie=choco; tasty_cookie="
+  )
+  expect_length(req$cookie, 1L)
+  expect_type(req$cookie, "list")
+  expect_equal(
+    req$cookie, 
+    list(
+      yummy_cookie = "choco"
+    )
+  )
+
+  # parser
+  fn <- function(req) {
+    return(req$HTTP_COOKIE)
+  }
+  parser <- as_cookie_parser(fn)
+  expect_snapshot(parser)
+  app <- Ambiorix$new()
+  app$use(parser)
+  req <- mockRequest(
+    cookie = "yummy_cookie=choco;"
+  )
+  expect_type(req$cookie, "character")
+  expect_equal(req$cookie, "yummy_cookie=choco;")
 })
