1728cffee23623452dd5306b8e2af8836c5cea10
John Coene <jcoenep@gmail.com>
2024-01-15 15:14:16 +0100

    fix: rejected promise

--- R/routing.R
+++ R/routing.R
@@ -419,7 +419,7 @@ Routing <- R6::R6Class(
                 onRejected = function(error){
                   message(error)
                   .globals$errorLog$log(req$REQUEST_METHOD, "on", req$PATH_INFO, "-", "Server error")
-                  private$.routes[[i]]$error(request, res)
+                  private$.routes[[i]]$error(request, res, error)
                 }
               )
             )
