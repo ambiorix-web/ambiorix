283206f088a6fc6941803daaef2f754db4ec01d5
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-02-24 21:44:07 +0300

    ch: test custom serialiser

--- tests/testthat/test-serialise.R
+++ tests/testthat/test-serialise.R
@@ -16,3 +16,27 @@ test_that("serialise", {
     yyjsonr::write_json_str(obj)
   )
 })
+
+test_that("custom serialiser works", {
+  global_serialiser <- getOption("AMBIORIX_SERIALISER")
+  on.exit(options(AMBIORIX_SERIALISER = global_serialiser))
+
+  my_serialiser <- function(data, ...) {
+    list(
+      data = data,
+      serialised = jsonlite::toJSON(x = data, ...)
+    )
+  }
+
+  options(AMBIORIX_SERIALISER = my_serialiser)
+
+  obj <- list(a = 1, b = "hello, world!")
+
+  expect_equal(
+    my_serialiser(obj),
+    list(
+      data = obj,
+      serialised = jsonlite::toJSON(obj)
+    )
+  )
+})
