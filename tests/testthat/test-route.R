5e306a05cd07311fa772575f3650690b834705d1
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-02-27 22:53:21 +0300

    ch: fix failing test on Route class

--- tests/testthat/test-route.R
+++ tests/testthat/test-route.R
@@ -1,8 +1,5 @@
 test_that("Route", {
-
-  r <- Route$new(
-    path = "/:id"
-  )
+  r <- Route$new(path = "/:id")$decompose()$as_pattern()
 
   expect_true(r$dynamic)
   expect_equal(r$pattern, "^/[[:alnum:][:space:][:punct:]]*$")
