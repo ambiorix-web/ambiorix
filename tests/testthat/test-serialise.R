4d7658150ddd90ab5f1ecc987f7951891de68bc8
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-10-07 12:04:16 +0300

    ch: extend serializer test coverage, closes #128

--- tests/testthat/test-serialise.R
+++ tests/testthat/test-serialise.R
@@ -17,6 +17,54 @@ test_that("serialise", {
   )
 })
 
+test_that("serialise falls back to default serialiser when option unset", {
+  obj <- list(a = 1, b = "hello, world!")
+  global_serialiser <- getOption("AMBIORIX_SERIALISER")
+  on.exit(options(AMBIORIX_SERIALISER = global_serialiser), add = TRUE)
+
+  options(AMBIORIX_SERIALISER = NULL)
+
+  expect_identical(
+    serialise(obj),
+    default_serialiser(obj)
+  )
+})
+
+test_that("serialise forwards dots to custom serialiser", {
+  obj <- list(a = 1)
+  global_serialiser <- getOption("AMBIORIX_SERIALISER")
+  on.exit(options(AMBIORIX_SERIALISER = global_serialiser), add = TRUE)
+
+  captured <- NULL
+  my_serialiser <- function(data, ...) {
+    captured <<- list(data = data, dots = list(...))
+    "custom"
+  }
+
+  options(AMBIORIX_SERIALISER = my_serialiser)
+
+  result <- serialise(obj, digits = 2, auto_unbox = FALSE)
+  expect_equal(result, "custom")
+  expect_equal(captured$data, obj)
+  expect_equal(captured$dots, list(digits = 2, auto_unbox = FALSE))
+})
+
+test_that("default serialiser gives precedence to dots over opts", {
+  obj <- list(a = 1)
+
+  expect_equal(
+    default_serialiser(
+      obj,
+      opts = list(auto_unbox = FALSE),
+      auto_unbox = TRUE
+    ),
+    yyjsonr::write_json_str(
+      obj,
+      opts = list(auto_unbox = TRUE)
+    )
+  )
+})
+
 test_that("custom serialiser works", {
   global_serialiser <- getOption("AMBIORIX_SERIALISER")
   on.exit(options(AMBIORIX_SERIALISER = global_serialiser))
