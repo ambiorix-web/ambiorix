263b594836255c7ca4d514f5c04c512bbae88f1f
Kennedy Mwavu <mwavukennedy@gmail.com>
2026-09-26 21:37:42 +0300

    ch: rm unnecessary test

--- tests/testthat/test-serialise.R
+++ tests/testthat/test-serialise.R
@@ -64,27 +64,3 @@ test_that("default serialiser gives precedence to dots over opts", {
     )
   )
 })
-
-test_that("custom serialiser works", {
-  global_serialiser <- getOption("AMBIORIX_SERIALISER")
-  on.exit(options(AMBIORIX_SERIALISER = global_serialiser))
-
-  my_serialiser <- function(data, ...) {
-    list(
-      data = data,
-      serialised = jsonlite::toJSON(x = data, ...)
-    )
-  }
-
-  options(AMBIORIX_SERIALISER = my_serialiser)
-
-  obj <- list(a = 1, b = "hello, world!")
-
-  expect_equal(
-    my_serialiser(obj),
-    list(
-      data = obj,
-      serialised = jsonlite::toJSON(obj)
-    )
-  )
-})
